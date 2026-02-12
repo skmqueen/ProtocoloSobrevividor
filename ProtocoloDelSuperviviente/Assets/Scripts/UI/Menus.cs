2e366b939ac4c20cb18d8f1993060ab0dcfaac99
Iria <iriagb04@gmail.com>
2026-02-12 12:12:03 +0000

    Clase 2 terminada

---
+++ ProtocoloDelSuperviviente/Assets/Scripts/UI/Menus.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Menus : MonoBehaviour
+{
+    public static Menus Instance;
+
+    void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        else
+            Destroy(gameObject);
+
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public void Jugar()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void Salir()
+    {
+        Debug.Log("Salir ... ");
+        Application.Quit();
+    }
+
+    public void Restart(string Juego)
+    {
+        SceneManager.LoadScene(Juego);
+    }
+
+    public void GameOver(string GameOver)
+    {
+        SceneManager.LoadScene(GameOver);
+    }
+
+    public void Victoria(string Victoria)
+    {
+        SceneManager.LoadScene(Victoria);
+    }
+}
